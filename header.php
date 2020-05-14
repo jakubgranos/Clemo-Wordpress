4922270e9f7b7639f7c6a5eed937c4dd6c9f4205
jakub <jakubgranos@gmail.com>
2020-05-14 22:40:55 +0200

    delete unnesesary files

--- header.php
+++ header.php
@@ -6,9 +6,9 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="<?php language_attributes();?>">
+<html <?php language_attributes();?>>
 <head>
-  <meta <?php bloginfo('charset')?>>
+  <meta charset="<?php bloginfo('charset')?>">
   <meta name="description" content="<?php bloginfo( 'description' );?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
--- includes/theme-settings/enqueue.php
+++ includes/theme-settings/enqueue.php
@@ -15,8 +15,6 @@ function clemo_load_scripts() {
     wp_enqueue_script( 'swiper.js', get_template_directory_uri() . '/js/swiper/swiper.min.js', array(), '1.0.0', true );
     wp_enqueue_script( 'vertical-slider.js', get_template_directory_uri() . '/js/swiper/vertical-slider.js', array(), '1.0.0', true );
     wp_enqueue_script( 'bootstrap.min.js', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '4.3.1', true );
-    wp_enqueue_script( 'jquery-3.3.1.slim.min.js', get_template_directory_uri() . '/js/jquery-3.3.1.slim.min.js', array( 'jquery' ), '3.3.1', true );
-    wp_enqueue_script( 'jquery.1.12.1.min.js', get_template_directory_uri() . '/js/jquery.1.12.1.min.js', array( 'jquery' ), '1.12.1', true );
     wp_enqueue_script( 'horizontal-slider.js', get_template_directory_uri() . '/js/swiper/horizontal-slider.js', array(), '1.0.0', true );
     wp_enqueue_script( 'client-opinion.js', get_template_directory_uri() . '/js/swiper/client-opinion.js', array(), '1.0.0', true );
 }
